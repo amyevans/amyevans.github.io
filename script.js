361f1db6f8c0cdf2c1aa3ff2194b56ea5fb53068
Amy Evans <amyevans87@gmail.com>
2015-09-23 13:18:13 -0500

    Import code from this project on codepen

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['home', 'about', 'portfolio', 'contact'],
+		menu: '#nav',
+	});
+
+	$('.responsive').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+});
+
+//Animate circles in Contact
+$('.anim').hover(
+	function() {
+		$(this).animate({
+			top: '-=10px'
+		});
+	},
+	function() {
+		$(this).animate({
+			top: '+=10px'
+		}, 'fast');
+	}
+);
