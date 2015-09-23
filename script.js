df211d7d8446f00d36722b7bbb8f11ee7176f1d9
Amy Evans <amyevans87@gmail.com>
2015-09-23 16:50:19 -0500

    Update portfolio

--- script.js
+++ script.js
@@ -8,27 +8,18 @@ $(document).ready(function() {
 		dots: true,
 		infinite: false,
 		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 		responsive: [
 			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 600,
+				breakpoint: 800,
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 2
 				}
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 600,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1
