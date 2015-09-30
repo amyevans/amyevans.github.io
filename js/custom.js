b93f7dbef106fccec57938180ced3bb6a6d8bc39
Amy Evans <amyevans87@gmail.com>
2015-09-30 12:44:52 -0500

    Overhaul

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+// Sticky Navbar
+$(document).ready(function() {
+    $(".navbar").sticky({topSpacing: 0});
+});
+
+// Flex Slider
+$(window).load(function() {
+    $('.main-flex-slider').flexslider({
+        slideshowSpeed: 5000,
+        directionNav: false,
+        animation: "fade",
+        controlNav: false
+    });
+});
+
+// Auto Close Responsive Navbar on Click
+function close_toggle() {
+    if ($(window).width() <= 768) {
+        $('.navbar-collapse a').on('click', function() {
+            $('.navbar-collapse').collapse('hide');
+        });
+    }
+    else {
+        $('.navbar .navbar-default a').off('click');
+    }
+}
+close_toggle();
+
+$(window).resize(close_toggle);
+
+//jQuery for page scrolling
+$(function() {
+    $('.navbar a,.btn,.to-top').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 50
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Parallax
+$(function() {
+    $.stellar({
+        horizontalScrolling: false
+    });
+});
+
+// WOW plugin for animate.css on scroll
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false        // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
